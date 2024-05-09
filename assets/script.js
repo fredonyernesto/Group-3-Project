99758fe7bd5accccfebbcced438cb4e3d64a5972
Sephora Jean <sephorejean@gmail.com>
2024-05-08 21:22:59 -0400

    Added API for weather data from cities

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+let city = document.querySelector('.search-bar');
+
+function appendCity(event){
+    city = document.querySelector('.search-bar').value;
+    console.log("City" + city)
+    apiUrl = ("https://wttr.in/" +  city + "?format=j1");
+    console.log("THis is the new city" + apiUrl);
+
+    fetch(apiUrl)
+    .then(response => {
+        if (!response.ok) {
+        throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('Data received:', data);
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
+}
+
+document.getElementById('submit').addEventListener('click', appendCity);
