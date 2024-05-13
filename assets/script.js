7f87dcc9f52da40dad5dfef1756f203bfcc19d18
SHMERNIE <fredony.ernesto@gmail.com>
2024-05-13 16:20:25 -0700

    Fifth Commit: two additional functions were implemented to enhance the functionality of the application. These functions enable the retrieveal of recipe information based on the category selected by the user.

--- assets/script.js
+++ assets/script.js
@@ -1,61 +1,83 @@
 let darkMode = false;
-const toggleButton = document.getElementById ('toggle')
-const body=document.body;
-toggleButton.addEventListener ('click' ,function(){
-    if (darkMode){
+const toggleButton = document.getElementById('toggle');
+const body = document.body;
+
+//Function to enable dark mode
+toggleButton.addEventListener('click', function () {
+    if (darkMode) {
         body.classList.remove('dark-mode');
-        darkMode=false;
-    }
-    else{
+        darkMode = false;
+    } else {
         body.classList.add('dark-mode');
-        darkMode=true;
+        darkMode = true;
     }
 });
 
 let city = document.querySelector('.search-bar');
+let categoryArr = [];
 
-//Function to fetch data from a given URL
+
+// Function to fetch data from a given URL
 function fetchData(url) {
     return fetch(url)
-    .then(response => {
-        if(!response.ok){
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .catch(error => {
-        console.error('There was a problem with the fetch operation', error);
-    });
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation', error);
+        });
 }
 
-//Function to append city data based on user input 
-function appendCity(event){
+// Function to append city data based on user input 
+function appendCity(event) {
     event.preventDefault();
 
     const city = document.querySelector('.search-bar').value;
-    const geoApiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=c3023f6bd0f4493002d6feb29e0f0be6`
+    const geoApiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=c3023f6bd0f4493002d6feb29e0f0be6`;
 
     fetchData(geoApiUrl)
         .then(data => {
             const lat = data[0].lat;
             const lon = data[0].lon;
-            const weatherApiUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=c3023f6bd0f4493002d6feb29e0f0be6`
-            return fetchData(weatherApiUrl)
+            const weatherApiUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=c3023f6bd0f4493002d6feb29e0f0be6`;
+            return fetchData(weatherApiUrl);
         })
         .then(weatherdata => {
             console.log('Weather Data received:', weatherdata);
         });
 }
 
-const foodUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata'
+const categoryList = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list';
 
-//Function to append food data
-function appendFood(){
-    fetchData(foodUrl)
+// Function to append food data
+// function appendCategoryList() {
+//     fetchData(categoryList)
+//         .then(data => {
+//             categoryArr = data.meals.map(item => Object.values(item));
+//             return categoryArr;
+//         })
+//         .then(() =>{
+//             console.log(categoryArr)
+//         })
+// }
+
+// Function to append category data 
+function appendCategory(event) {
+    let selectedCategory = event.target.value;
+    let categoryUrl = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${selectedCategory}`;
+    fetchData(categoryUrl)
         .then(data => {
-            console.log('Food Data received:', data);
-        });
+            console.log('Category Data received:', data);
+        })
 }
 
-appendFood();
+function appendButton(){
+    let button = document.getElementById('taste-switch');
+    button.addEventListener('click', appendCategory);
+}
+
+appendButton();
 document.getElementById('submit').addEventListener('click', appendCity);
